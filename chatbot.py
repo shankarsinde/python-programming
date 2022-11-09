1800e50d59f2356204e728d9376291145694b2aa
shankar <shankarsinde2681@gmail.com>
2022-11-09 23:03:19 +0530

    using first file

---
+++ chatbot.py
@@ -0,0 +1,19 @@
+# designing a shivamogga helpline service chartbot
+print("Welcome to shivamogga help line service")
+print("How can we help you")
+print("press 1 for Electicity problem")
+print("Press 2 for Road problem")
+print("Press 3 for Drinage problem")
+print("Press 4 for Neighbour's problem")
+print("press 5 for other problems")
+# accepting the user input
+userinput = int(input("Select your problem number"))
+# using nested if condition for taking the user input
+if (userinput == 1):
+
+     if userinput == 1:
+         print("Electricity problem of House")
+     elif userinput == 2:
+         print("abcd")
+
+
