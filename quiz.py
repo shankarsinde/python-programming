9cff4b61f9fb4e57e81f4093abe400d2302a3e08
shankar <shankarsinde2681@gmail.com>
2022-11-10 21:04:47 +0530

    using first file

---
+++ quiz.py
@@ -0,0 +1,90 @@
+print("Hi, welcome to our quiz game")
+Name = input("What is you Name:")
+Branch = input("your Branch:")
+USN = input("usN no.")
+one = input("what is the date of birth of Veer Savarkar")
+two = input("where was Veer Savarkar Born:")
+three = input("when did savarkar involved in indian independence:")
+four = input("which organisation built by Veer Savarkar to fight with British: ")
+five = input("when did the above organisation was built:")
+six = input("In which jail Savarkar was given the kalapani british:")
+seven = input("How many years he was sentenced to imprisonment:")
+eight = input("what is the name of the temple buit by Veer Savarkar for the  lower caste people:")
+nine = input("When it was Bulit:")
+ten = input(" when did savrkar  died:")
+one_a = "1883"
+one_b = "1890"
+one_c = "1850"
+two_a = "Bhagur Mharashtra"
+two_b = "Bombay Mharashtra"
+two_c = "pune Mharashtra"
+three_a = "1909"
+three_b = "1904"
+three_c = "1910"
+four_a = "Guptha Bhartha"
+four_b = "Abhinav Bharat"
+four_c = "Hindusthan socialist"
+five_a = "1905"
+five_b = "1908"
+five_c = "1904"
+six_a = "Andaman nicobar"
+six_b = "central jail guwahati"
+six_c = "Madras central prision"
+seven_a = "50"
+seven_b = "25"
+seven_c = "35"
+eight_a = "patita pavana mandir"
+eight_b = "Adi vinayagar Temple"
+eight_c = "Manimoorthe Swaram Temple"
+nine_a = "1920"
+nine_b = "1931"
+nine_c = "1948"
+ten_a = "1966"
+ten_b = "1970"
+ten_c = "1964"
+if one == one_a:
+    print("1.correct")
+else:
+    print("1.Incorrect  1983 : is the correct answer")
+if two == one_a:
+    print("2.correct")
+else:
+    print("2.Incorrect : Bhagpur is the correct answer")
+if three == three_b:
+    print("3.correct")
+else:
+    print("3.Incorrect : 1904 is the correct answer")
+if four == four_b:
+    print("4.correct")
+else:
+    print("4.Incorrect : Abinav Bharat is the correct answer")
+if five == five_c:
+    print("5.correct")
+else:
+    print(" 5.Incorrect  : 1904 is the correct answer")
+if six == six_a:
+    print("6.correct")
+else:
+    print("6.Incorrect : Andaman nicobar is the correct answer")
+if seven == seven_a:
+    print("7.correct")
+else:
+    print("7.Incorrect : 1950 is the correct answer")
+if eight == eight:
+    print("8.corrct")
+else:
+    print("8.Incorrect : patita pavana mandir is the correct answer")
+if nine == nine_b:
+    print("9.correct")
+else:
+    print(" 9.Incorrect : 1931 is the correct answer")
+if ten == ten_a:
+    print("10.correct")
+else:
+    print("10.Incorrect : 1966 is the correct answer")
+print("Thank you"+ Name + "for completing the quiz")
+
+
+
+
+
